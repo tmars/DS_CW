ee6430f1fde9c451b3c4b6c7514091a348a7957c
talipov_mi_talipov.mars <t.mars@mail.ru>
2014-05-26 17:43:14 +0300

    created center_sys and office_sys

---
+++ office_sys/catalog/models.py
@@ -0,0 +1,49 @@
+#coding=utf8
+
+from django.db import models
+
+BODY_CHOICE = (
+    ('u', u'Универсал'),
+    ('h', u'Хетчбек'),
+    ('s', u'Седан'),
+)
+
+CLASS_CHOICE = (
+    ('l', u'Эконом класс'),
+    ('m', u'Средний класс'),
+    ('h', u'Бизнес класс'),
+)
+
+class Tariff(models.Model):
+    name = models.CharField(max_length=50, verbose_name=u'Название тарифа')
+    min_days = models.IntegerField(verbose_name=u'Минимальное количетсво дней')
+    max_days = models.IntegerField(verbose_name=u'Максимальное количетсво дней')
+    
+    def __unicode__(self):
+        return self.name
+    
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=50, verbose_name=u'Название бренда')
+
+    def __unicode__(self):
+        return self.brand_name
+    
+class Car(models.Model):
+    brand = models.ForeignKey(Brand, blank=False, null=False)
+    model_name = models.CharField(max_length=100,verbose_name=u'Модель')
+    pub_date = models.DateTimeField('date published')
+    body_type = models.CharField(max_length=1, choices=BODY_CHOICE, verbose_name=u'Тип кузова')
+    class_type = models.CharField(max_length=1, choices=CLASS_CHOICE, verbose_name=u'Класс')
+    image = models.ImageField(upload_to="images/cars", verbose_name=u'Изображение')
+    
+    tarif1 = models.IntegerField(verbose_name=u'тариф на сутки')
+    tarif2 = models.IntegerField(verbose_name=u'тариф на период от 3 до 15 суток')
+    tarif3 = models.IntegerField(verbose_name=u'тариф на период от 15 до 30 суток')
+    tarif4 = models.IntegerField(verbose_name=u'тариф на период свыше 30 суток')
+    
+    
+    def __unicode__(self):
+        return "%s %s" % (self.brand, self.model_name)
+            
+ 
+    
---
+++ office_sys/office_sys/settings.py
@@ -0,0 +1,90 @@
+"""
+Django settings for office_sys project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'l-yrgcs5yp%k8ba@8yr8*mmpe=tfx*(*lgoc)0k4@=!u_9gkuz'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'catalog',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'office_sys.urls'
+
+WSGI_APPLICATION = 'office_sys.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rsoi_office_sys',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'ru-ru'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
+MEDIA_URL = '/media/'
